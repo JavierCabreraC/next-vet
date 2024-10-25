f360d3bdfc9827c2b9e5cf97d3ee540872b66619
Javier Cabrera <fcabreraclaros@gmail.com>
2024-10-25 04:52:31 -0400

    endpoints changed to railway

--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -29,7 +29,7 @@ export const LoginForm: React.FC<LoginFormProps> = ({ onClose }) => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:3333/api/auth/login', {
+            const response = await fetch('https://neon-demo-production.up.railway.app/api/auth/login', {
                 method: 'POST',
                 headers: {
                   'Content-Type': 'application/json',
