27791bbb6777af5ae7272ef6bebad5f90027e8a7
Javier Cabrera <fcabreraclaros@gmail.com>
2024-10-31 03:57:47 -0400

    endpoits fixed

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,16 +1,16 @@
 export const API_CONFIG = {
     BASE_URL: 'http://localhost:3333/api',
     ENDPOINTS: {
-        ADM_PERSONAL:   '/admin/personal',
-        ADM_CLIENTES:   '/admin/clientes',
-        ADM_MASCOTAS:   '/admin/mascotas',
-        ADM_LOGS:       '/admin/logs',
-        CLI_MASCOTAS:   '/cliente/mascotas',
-        CLI_RESERVA:    '/cliente/reservaciones',
-        CLI_RESERVAGRAL:    '/cliente/reservaciones/gral',
-        CLI_RESERV:     '/cliente/reservacion',
-        DOC_VACUNAS:    '/vetdoc/vacunas',
-        DOC_REGVAC:     '/vetdoc/regvac'
+        ADM_PERSONAL:          '/admin/personal',
+        ADM_CLIENTES:          '/admin/clientes',
+        ADM_MASCOTAS:          '/admin/mascotas',
+        ADM_LOGS:              '/admin/logs',
+        CLI_MASCOTAS:          '/cliente/mascotas',
+        CLI_RESERVA:           '/cliente/reservacion',
+        CLI_RESERVACLI:        '/cliente/reservacion/cli',
+        CLI_RESERVAGRAL:       '/cliente/reservacion/gral',
+        DOC_VACUNAS:           '/vetdoc/vacunas',
+        DOC_REGVAC:            '/vetdoc/regvac'
     },
     ITEMS_PER_PAGE: 6
 } as const;
